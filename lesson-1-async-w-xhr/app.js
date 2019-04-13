0229f4083e19a8498a3941f00ce5400ae501d40d
Khaled Alissa <khaledalissa@Khaleds-Mac-mini.local>
2019-04-13 11:03:12 +0300

    first commit

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,27 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        console.log('submitted');
+
+        searchFor(searchedForTest);
     });
 })();
+
+
+function addImage(){
+
+}
+
+function searchFor(text){
+    
+    const unsplashRequest = new XMLHttpRequest();
+    
+    unsplashRequest.open('GET', 'https://api.unsplash.com/search/photos?page=1&query=${text}')
+    
+    unsplashRequest.onload = addImage;
+    
+    unsplashRequest = addImage;
+    
+    unsplashRequest.send();
+}
